baf43c8f4684282548281583d678dce1ad01da6e
Luis Cuzquen <cuzquenl@la-z-boyaz.com>
2019-10-22 10:19:54 -0700

    Updates

---
+++ precache-manifest.c2b57253822dff626a90526f39bb0deb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee67f49001825a9f6dcd71a79d84dd76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa54c6578c9a6a8521d9",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "f83c18d66b658e1aaf0e",
+    "url": "/static/css/main.53a3db6b.chunk.css"
+  },
+  {
+    "revision": "fa54c6578c9a6a8521d9",
+    "url": "/static/js/2.e15116a3.chunk.js"
+  },
+  {
+    "revision": "f83c18d66b658e1aaf0e",
+    "url": "/static/js/main.e7b2582f.chunk.js"
+  },
+  {
+    "revision": "1da9d01a731fa29a7f2a",
+    "url": "/static/js/runtime~main.fea4d413.js"
+  }
+]);
